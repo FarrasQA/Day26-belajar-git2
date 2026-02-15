6608f260639dd30a06282bf0beb7726db739697e
FarrasQA <farrasasshidiq5@gmail.com>
2026-02-15 12:04:07 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba GIT remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt dolore, repudiandae deleniti explicabo aut eaque qui quae delectus ducimus laboriosam impedit velit id labore, fuga, earum quam unde? Doloribus, culpa!</p1>
+</body>
+</html>
